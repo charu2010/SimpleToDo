800ef53166bb53e76e4969df0a19bab7c2f82d9c
charu <charu.agwl@gmail.com>
2016-06-29 00:23:59 -0700

    ToDoApp

---
+++ app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -0,0 +1,140 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private GoogleApiClient client;
+    private final int REQUEST_CODE = 20;
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdaptor;
+    ListView lvItimes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lvItimes=(ListView)findViewById(R.id.lvItems);
+        //items=new ArrayList<>();
+        readItems();
+        itemsAdaptor=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
+        lvItimes.setAdapter(itemsAdaptor);
+        setupListViewListener();
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+    }
+
+
+    private void setupListViewListener() {
+        lvItimes.setOnItemLongClickListener
+                (
+                        new AdapterView.OnItemLongClickListener() {
+                            @Override
+                            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                                items.remove(pos);
+                                itemsAdaptor.notifyDataSetChanged();
+                                writeItems();
+                                return true;
+                            }
+                        });
+       lvItimes.setOnItemClickListener(
+              new AdapterView.OnItemClickListener() {
+                @Override
+              public void onItemClick(AdapterView<?> padapter, View item, int pos, long id) {
+
+                      Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                    i.putExtra("position", pos);
+                    i.putExtra("value", items.get(pos).toString());
+                     startActivityForResult(i, REQUEST_CODE);
+           }
+                }
+        );
+    }
+
+    public void onAddItem(View V) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdaptor.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.ruchika.mytodoapp/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.codepath.simpletodo/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+}
